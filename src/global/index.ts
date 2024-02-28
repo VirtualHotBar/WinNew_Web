4c8cbbf8aa8d32b6a4fa6a2966a85baa69a06a22
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-02-28 18:02:38 +0800

    Update index.ts

--- src/global/index.ts
+++ src/global/index.ts
@@ -1,5 +1,10 @@
-document.documentElement.removeAttribute('theme-mode');
+
+//51la
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"KMXcXkix0m3qBlgg\",ck:\"KMXcXkix0m3qBlgg\"});</script>")
+
+
 //设置颜色模式
+document.documentElement.removeAttribute('theme-mode');
 /*   function matchMode() {
     // detect if on dark mode
     var isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
